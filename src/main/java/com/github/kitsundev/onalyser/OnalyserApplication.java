22979e063cb3cf699f5be8bc7ecac706bf01ad74
CodeNekomancer <codenekomancer@gmail.com>
2022-03-03 16:42:54 +0100

    Initial Commit

---
+++ src/main/java/com/github/kitsundev/onalyser/OnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.github.kitsundev.onalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kitsundev/onalyser/OnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kitsundev.onalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
